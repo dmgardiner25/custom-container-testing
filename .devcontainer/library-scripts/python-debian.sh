3999b52c7f1bd05d37596a51b90ff17a0a26d9e2
David Gardiner <dmgardiner25@gmail.com>
2021-02-10 17:09:25 +0000

    Escape $

--- .devcontainer/library-scripts/python-debian.sh
+++ .devcontainer/library-scripts/python-debian.sh
@@ -144,6 +144,6 @@ export PIPX_BIN_DIR="${PIPX_BIN_DIR}"
 export PYENV_ROOT="${PYENV_ROOT}"
 if [[ "\${PATH}" != *"\${PIPX_BIN_DIR}"* ]]; then export PATH="\${PATH}:\${PIPX_BIN_DIR}"; fi
 if [[ "\${PATH}" != *"\${PYENV_ROOT}/bin"* ]]; then export PATH="\${PATH}:\${PYENV_ROOT}/bin"; fi
-if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi
+if command -v pyenv 1>/dev/null 2>&1; then\n  eval "\$(pyenv init -)"\nfi
 EOF
 )"
